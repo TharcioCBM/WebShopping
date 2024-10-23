c3f81a63646d37f252969a8b189f2c9d1da4a8a3
Jose-Luan <joseluanfs@gmail.com>
2024-10-23 19:59:35 -0300

    feat: Implementation of registration logic and API integration- Adds input states for username, email and password in RegisterModal.- Implements the POST request for the registration route (/api/auth/register).- Adds error handling and feedback messages to the user in case of registration failure.- Uses the useRouter hook to redirect the user after successful registration.

---
+++ front-end/src/pages/api/auth/register.ts
@@ -0,0 +1,32 @@
+// src/app/api/auth/register.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function register(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    try {
+      const { username, email, password } = req.body;
+
+      // Realiza a requisição POST para a API do Flask
+      const response = await fetch('http://127.0.0.1:8080/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        return res.status(201).json({ message: 'Usuário cadastrado com sucesso!', data });
+      } else {
+        const errorData = await response.json();
+        return res.status(response.status).json({ message: errorData.error_message });
+      }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      return res.status(500).json({ message: 'Erro no servidor. Tente novamente mais tarde.' });
+    }
+  } else {
+    return res.status(405).json({ message: 'Método não permitido' });
+  }
+}
