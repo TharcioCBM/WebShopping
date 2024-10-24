67839b9a31a3678631b0634a8e868e01b1d34a02
TharcioCBM <camboim_@hotmail.com>
2024-10-24 16:58:50 -0300

    feat: back-end implementation, feature: user authentication and login; using JSON Web Tokens (JWT) to generate authentication tokens. update: Some changes to the names of some functions and attributes of the responses.

--- api/config.py
+++ api/config.py
@@ -7,6 +7,7 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or 'this is a secret'
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    JSON_SORT_KEYS=False
+    
 class DevelopmentConfig(Config):
     DEBUG = True
