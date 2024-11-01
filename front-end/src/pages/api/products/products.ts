cbc7eeeb9914ae5a4a16c9410f4c515ea660df82
Jose-Luan <joseluanfs@gmail.com>
2024-11-01 18:50:54 -0300

    feat: Modify API handler to support fetching individual product details

--- front-end/src/pages/api/products/products.ts
+++ front-end/src/pages/api/products/products.ts
@@ -3,17 +3,23 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         try {
-            const response = await fetch('http://127.0.0.1:8080/product/offer', {
+            const productId = req.query['x-product-id']
+
+            const url = productId 
+                ? `http://127.0.0.1:8080/product/${productId}`
+                : 'http://127.0.0.1:8080/product/offer';
+
+            const response = await fetch(url, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
                 },
             });
+
             const data = await response.json();
 
             if (response.ok) {
                 res.status(200).json(data);
-                //console.log(data);
             } else {
                 res.status(response.status).json({ message: data.message });
             }
